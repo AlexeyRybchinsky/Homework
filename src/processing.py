a94bdcda348efd11302e355b942f890da0bf8c2a
AlexeyRybchinsky <exicutor666@gmail.com>
2025-08-16 01:30:40 +0300

    fixes processing file

--- src/processing.py
+++ src/processing.py
@@ -4,13 +4,14 @@ from typing import Any, Dict, List
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
-    Фильтрует список словарей по значению ключа 'state'.
+    Возвращает новый список словарей, в котором только те элементы,
+    у которых значение ключа 'state' совпадает с переданным.
     """
     return [item for item in data if item.get("state") == state]
 
 
 def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
     """
-    Сортирует список словарей по ключу 'date'.
+    Возвращает новый список словарей, отсортированный по ключу 'date'.
     """
     return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=descending)
